c77643c53dc326dad16a63f5a91a30c3745e1f7b
Luca Del Puppo <Puppo@users.noreply.github.com>
2026-05-07 09:22:11 +0200

    fix: update type checks in tests to prevent callable errors

--- types/index.tst.ts
+++ types/index.tst.ts
@@ -1,14 +1,14 @@
 import { expect } from 'tstyche'
 import sjson from '..'
 
-// expectError(sjson.parse(null)) - tstyche: TypeScript would error on this
+expect(sjson.parse).type.not.toBeCallableWith(null)
 expect(sjson.parse('{"anything":0}')).type.toBe<any>()
 
 sjson.parse('"test"', null, { protoAction: 'remove' })
-// expectError(sjson.parse('"test"', null, { protoAction: 'incorrect' })) - TypeScript would error
+expect(sjson.parse).type.not.toBeCallableWith('"test"', null, { protoAction: 'incorrect' })
 sjson.parse('"test"', null, { constructorAction: 'ignore' })
-// expectError(sjson.parse('"test"', null, { constructorAction: 'incorrect' })) - TypeScript would error
-// expectError(sjson.parse('"test"', { constructorAction: 'incorrect' })) - TypeScript would error
+expect(sjson.parse).type.not.toBeCallableWith('"test"', null, { constructorAction: 'incorrect' })
+expect(sjson.parse).type.not.toBeCallableWith('"test"', { constructorAction: 'incorrect' })
 sjson.parse('test', { constructorAction: 'remove' })
 sjson.parse('test', { protoAction: 'ignore' })
 sjson.parse('test', () => {}, { protoAction: 'ignore', constructorAction: 'remove' })
@@ -16,11 +16,11 @@ sjson.parse('"test"', null, { safe: true })
 sjson.parse('"test"', { safe: true })
 sjson.parse('test', () => {}, { safe: false })
 sjson.parse('test', { protoAction: 'remove', safe: true })
-// expectError(sjson.parse('"test"', null, { safe: 'incorrect' })) - TypeScript would error
+expect(sjson.parse).type.not.toBeCallableWith('"test"', null, { safe: 'incorrect' })
 
 sjson.safeParse('"test"', null)
 sjson.safeParse('"test"')
-// expectError(sjson.safeParse(null)) - TypeScript would error
+expect(sjson.safeParse).type.not.toBeCallableWith(null)
 
 sjson.scan({}, { protoAction: 'remove' })
 sjson.scan({}, { protoAction: 'ignore' })
@@ -29,7 +29,7 @@ sjson.scan({}, { constructorAction: 'ignore' })
 sjson.scan([], {})
 sjson.scan({}, { safe: true })
 sjson.scan({}, { protoAction: 'remove', safe: false })
-// expectError(sjson.scan({}, { safe: 'incorrect' })) - TypeScript would error
+expect(sjson.scan).type.not.toBeCallableWith({}, { safe: 'incorrect' })
 
 declare const input: Buffer
 sjson.parse(input)
